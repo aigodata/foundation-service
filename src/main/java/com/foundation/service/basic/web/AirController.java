41e8d342614342275e103ad086f060e14e382b54
mengxiangyun <718517636@qq.com>
2019-09-09 11:28:41 +0800

    feat: save old keyiv in session

--- src/main/java/com/foundation/service/basic/web/AirController.java
+++ src/main/java/com/foundation/service/basic/web/AirController.java
@@ -3,6 +3,8 @@ package com.foundation.service.basic.web;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.foundation.service.basic.common.exception.GlobalException;
 import com.foundation.service.basic.common.util.Encryption;
 import com.foundation.service.basic.model.ResultModel;
+import com.foundation.service.basic.model.ResultModel.ResultStatus;
 import com.github.mengxianun.core.DataResultSet;
 import com.github.mengxianun.core.Translator;
 import com.google.common.collect.ImmutableMap;
@@ -19,7 +23,10 @@ import com.google.common.collect.ImmutableMap;
 @RestController
 public class AirController {
 
-	private static final String keyiv = "keyiv";
+	private static final Logger logger = LoggerFactory.getLogger(AirController.class);
+
+	private static final String OLD_KEY_NAME = "oldKey";
+	private static final String KEY_NAME = "key";
 
 	@Value("${service.air.crypt:false}")
 	private boolean crypt;
@@ -30,8 +37,7 @@ public class AirController {
 	@PostMapping("/action")
 	public ResultModel action(@RequestBody String requestJson, HttpSession session) throws Exception {
 		if (crypt) {
-			String currentKeyiv = getCurrentKey(session);
-			requestJson = Encryption.desEncrypt(requestJson, currentKeyiv, currentKeyiv).trim();
+			requestJson = decrypt(requestJson, session);
 		}
 		DataResultSet dataResultSet = translator.translate(requestJson);
 		if (!dataResultSet.succeed()) {
@@ -39,26 +45,52 @@ public class AirController {
 		}
 		Object data = dataResultSet.getData();
 		if (crypt) {
-			String currentKeyiv = getCurrentKey(session);
-			data = Encryption.encrypt(data.toString(), currentKeyiv, currentKeyiv);
+			data = encrypt(data, session);
 		}
 		return ResultModel.success(data);
 	}
 
 	@GetMapping("/action/key")
 	public ResultModel getKey(HttpSession session) {
+		Object currentKeyiv = session.getAttribute(KEY_NAME);
 		String newKey = getNewKey();
-		session.setAttribute(keyiv, newKey);
+		session.setAttribute(KEY_NAME, newKey);
+		session.setAttribute(OLD_KEY_NAME, String.valueOf(currentKeyiv));
 		return ResultModel.success(ImmutableMap.of("key", newKey, "iv", newKey));
 	}
 
-	private String getCurrentKey(HttpSession session) {
-		Object currentKeyiv = session.getAttribute(keyiv);
-		if (currentKeyiv == null || currentKeyiv.toString().isEmpty()) {
-			currentKeyiv = getNewKey();
-			session.setAttribute(keyiv, currentKeyiv.toString());
+	private String encrypt(Object data, HttpSession session) {
+		Object currentKeyivValue = session.getAttribute(KEY_NAME);
+		try {
+			return Encryption.encrypt(data.toString(), currentKeyivValue.toString(), currentKeyivValue.toString());
+		} catch (Exception e) {
+			logger.error("Request data encrypt failed", e);
+			throw new GlobalException(ResultStatus.SERVER_ERROR);
 		}
-		return currentKeyiv.toString();
+	}
+
+	private String decrypt(String requestJson, HttpSession session) {
+		String decrypted = decrypt(requestJson, session.getAttribute(KEY_NAME));
+		if (decrypted == null || decrypted.isEmpty()) {
+			decrypted = decrypt(requestJson, session.getAttribute(OLD_KEY_NAME));
+		}
+		if (decrypted == null || decrypted.isEmpty()) {
+			throw new GlobalException(ResultStatus.BAD_REQUEST.code(), "Request data decrypt failed");
+		}
+		return decrypted;
+	}
+
+	private String decrypt(String requestJson, Object keyivValue) {
+		try {
+			String decrypted = Encryption.desEncrypt(requestJson, keyivValue.toString(), keyivValue.toString()).trim();
+			if (decrypted.startsWith("{") && decrypted.endsWith("}")) {
+				return decrypted;
+			}
+		} catch (Exception e) {
+			logger.error("Request data decrypt failed", e);
+			return null;
+		}
+		return null;
 	}
 
 	private String getNewKey() {
